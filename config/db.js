e317bac559e0341bdf9578ff99fcbe8acd6c2558
Rahul-college-use <rahulcollegeuse@gmail.com>
2026-05-02 23:11:04 +0530

    1

--- config/db.js
+++ config/db.js
@@ -1,12 +1,11 @@
 const mongoose = require('mongoose');
 
-const password = encodeURIComponent("HFmWxA3kfWMdqHb7");
 
 const url1 = "mongodb+srv://GECJ:gec123@gecj.o4nhqas.mongodb.net/?appName=GECj"
 // const url2 = "mongodb://localhost:27017"
 const connectdb = async () => {
   try {
-    await mongoose.connect(`${url1}/college`);
+    await mongoose.connect(`${url1}`);
     console.log("MongoDB Connected");
   } catch (error) {
     console.error("MongoDB connection failed:", error);
